424a02bf78599faac10b750b596b8855ad98314d
clifford <clifford@noukod.com>
2018-09-04 20:30:37 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,6 @@
 # Your code goes here!
   class Dog
-    def name=(dogs_name)
-      @name = dogs_name
-    end
-
-    def name
-      @name
-    end
-
   end
-
-  class Dog
-    def bark
-      puts "woof!"
-    end
-  end
+fido.name
+fido.bark
+woof!
