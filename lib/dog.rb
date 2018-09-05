749b794a6980b44975bd692a023940e0dda6e646
Rudy Ravelin <rudy@noukod.com>
2018-09-05 17:47:37 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Dog
+  def name=(dog_name)
+  @name = dog_name
+end
+
+def name
+  @name
+end
+
+fido = Dog.new
+fido.name = "Fido"
+
+def bark
+  puts "woof!"
+end
+end
