9ef74e08ef08b5cc30945167ebb0063dedde8431
Saint-Jean Germila <germila@noukod.com>
2018-09-06 17:48:18 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
-# Your code goes here!
+class Dog
+
+  def name=(name)
+    @name = name
+  end
+
+   def name
+     @name
+   end
+
+def bark=(bark = "woof!")
+  @bark = bark
+  end
+
+def bark
+     @bark
+  end
+
+end
+
+
+fido = Dog.new
+fido.name = "Fido"
+
+fido.name
+
+fido.instance_variable_set("woof!")
+fido.bark
